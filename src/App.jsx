bb16e3f940d21e14c52f72e223717ae4c72766fc
nyspd <51014321+nyspd@users.noreply.github.com>
2025-07-12 14:43:54 +1200

    Delete src directory

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div style={{ color: 'white', background: '#121212', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>FreddieGPT React App is Running</h1>
-    </div>
-  );
-}
